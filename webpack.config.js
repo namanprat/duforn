ebe21cca9bb43c6fdf5c8737924c0bceeafa5151
namanprat <59867991+namanprat@users.noreply.github.com>
2024-02-15 14:57:13 +0530

    -

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,32 @@
-plugins: [
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let htmlPageNames = ['index', 'about', 'work'];
+let multipleHtmlPlugins = htmlPageNames.map(name => {
+  return new HtmlWebpackPlugin({
+    template: `./${name}.html`, // relative path to the HTML files
+    filename: `${name}.html`, // output HTML files
+    chunks: [`${name}`] // respective JS files
+  })
+});
+
+module.exports = {
+  entry: {
+    main: './main.js',
+    example1: './gsap.js',
+    example2: './script.js',
+    example3: './threedee.js',
+
+    //... repeat until example 4
+  },
+  plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './index.html'
+      template: "./index.html",
+      chunks: ['main']
     }),
     new HtmlWebpackPlugin({
-      filename: 'about.html',
-      template: './about.html'
-    }),
-    new CleanWebpackPlugin(['dist'])
-  ]
+        template: "./about.html",
+        chunks: ['main']
+      })
+  ].concat(multipleHtmlPlugins)
+  
+};
