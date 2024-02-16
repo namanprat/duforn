ddfbebec6a2378d6d0ef9ba514802f68db83db2a
namanprat <59867991+namanprat@users.noreply.github.com>
2024-02-17 00:53:25 +0530

    -

--- threedee.js
+++ threedee.js
@@ -1,18 +1,21 @@
 import "./index.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+console.log( TextGeometry );
+console.log( GLTFLoader );
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#logo'),
+});
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
 
 const textureLoader = new THREE.TextureLoader();
 const myTexture = textureLoader.load("/texture.jpeg");
