70e74a0a2fc659f724f024649bb5332b58c6039e
namanprat <59867991+namanprat@users.noreply.github.com>
2024-06-21 10:31:52 +0530

    --

--- logo-3d.js
+++ logo-3d.js
@@ -1,50 +1,50 @@
-import "./index.css";
 import * as THREE from "three";
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const dLoader = new DRACOLoader();
+dLoader.setDecoderPath("https://www.gstatic.com/draco/versioned/decoders/1.5.7/");
+dLoader.setDecoderConfig({ type: "js" });
+
 const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dLoader);
+
 const scene = new THREE.Scene();
 const hdriLoader = new RGBELoader();
-hdriLoader.load( './env.hdr', function (texture) {
-texture.mapping = THREE.EquirectangularReflectionMapping;
-scene.environment = texture;
+hdriLoader.load("./env.hdr", (texture) => {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.environment = texture;
 });
 
-//Camera
-var camera = new THREE.PerspectiveCamera( 15,  window.innerWidth / window.innerHeight, 0.1, 100 );
+const camera = new THREE.PerspectiveCamera(
+  15,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
 camera.position.z = 0.135;
-// camera.position.y = 0.5;
-
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#logo-model'),
-  alpha: true,antialias: true,
+  canvas: document.querySelector("#logo-model"),
+  alpha: true,
+  antialias: true,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 
-//RESIZE
 const resize = () => {
   camera.aspect = window.innerWidth / window.innerHeight;
-  console.log(camera.aspect);
-
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-dLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.7/');
-dLoader.setDecoderConfig({type:'js'});
-gltfLoader.setDRACOLoader(dLoader);
+};
 
 const group = new THREE.Group();
 scene.add(group);
 
-gltfLoader.load('./newlogo.glb', function(glb){
-  group.add(glb.scene); 
+gltfLoader.load("./newlogo.glb", (glb) => {
+  group.add(glb.scene);
 });
 
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -58,10 +58,9 @@ controls.update();
 const render = () => {
   controls.update();
   group.rotation.set(0, window.scrollY * 0.001, 0);
-
   requestAnimationFrame(render);
   renderer.render(scene, camera);
-}
+};
 
 render();
-window.addEventListener('resize', resize);
+window.addEventListener("resize", resize);
