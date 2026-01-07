c8e5e745e294ff0c114c511bf846dafca69a2ed8
namanprat <59867991+namanprat@users.noreply.github.com>
2026-01-07 13:22:40 +0530

    -

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,28 @@
-
-// vite.config js
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname,'index.html'),
-                about: resolve(__dirname,'about.html'),
-                work: resolve(__dirname,'work.html'),
-                contact: resolve(__dirname,'contact.html'),
-                project: resolve(__dirname,'project.html'),
-
-
-
-        },
+  optimizeDeps: {
+    include: ['postprocessing', 'three'],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        work: resolve(__dirname, "work.html"),
+        about: resolve(__dirname, "about.html"),
+        directors: resolve(__dirname, "directors.html"),
+        contact: resolve(__dirname, "contact.html"),
+        film: resolve(__dirname, "film.html"),
+      },
     },
-},
+    assetsInclude: [
+      "**/*.jpeg",
+      "**/*.jpg",
+      "**/*.png",
+      "**/*.svg",
+      "**/*.gif",
+    ],
+    copyPublicDir: true,
+  },
 });
